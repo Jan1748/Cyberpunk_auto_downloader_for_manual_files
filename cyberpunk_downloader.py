71ccc4eb34176d31cff7b103c3afddaeb739eb9c
jan1748 <jan1748@szut.de>
2020-12-25 12:44:12 +0100

    Now also recognises existing files without build number

--- cyberpunk_downloader.py
+++ cyberpunk_downloader.py
@@ -19,7 +19,7 @@ def start_download():
         Path(path).mkdir(parents=True, exist_ok=True)
         files_in_directory = os.listdir(path)
         print('Start downloading Game files')
-        download_all_files(url=url, installer_base=installer_base, gog_al_cookie=gog_al_cookie, thread_amount=thread_amount, files_in_directory=files_in_directory, path=path)  # Download Game Files
+        #download_all_files(url=url, installer_base=installer_base, gog_al_cookie=gog_al_cookie, thread_amount=thread_amount, files_in_directory=files_in_directory, path=path)  # Download Game Files
         print('All Game Installer Downloaded')
         print('Start downloading Patch files')
         download_all_files(url=url, installer_base=patch_base, gog_al_cookie=gog_al_cookie, thread_amount=thread_amount, files_in_directory=files_in_directory, path=path)  # Download Patch Files
@@ -58,6 +58,12 @@ def check_file_exists(path: str, file_name: str, files_in_directory):
         elif 'setup' in file_name:
             to_search.append(file_name.split('build')[0])
         to_search.append('-' + file_name.split('-')[1])
+    elif '.exe' in file_name:
+        if 'patch' in file_name:
+            to_search.append(file_name.split('Build')[0])
+        elif 'setup' in file_name:
+            to_search.append(file_name.split('build')[0])
+        to_search.append('.exe')
     for file in files_in_directory:
         counter = 0
         for p in to_search:
