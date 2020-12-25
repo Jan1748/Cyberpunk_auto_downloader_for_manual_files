b4148f43ee2514457a813a6a537d862bba53d4b5
jan1748 <jan1748@szut.de>
2020-12-25 11:39:12 +0100

    Now also recognises existing files without build number

--- cyberpunk_downloader.py
+++ cyberpunk_downloader.py
@@ -13,14 +13,16 @@ def start_download():
     url = 'https://www.gog.com/downloads/cyberpunk_2077_game'
     installer_base = '/en1installer'
     patch_base = '/en1patch'
+    path = 'Cyberpunk_2077'
     if check_cookie(url=url, installer_base=installer_base, gog_al_cookie=gog_al_cookie):
         thread_amount = int(input('How many files to download at the same time? (Ca. 20 Mb per file)'))
-        Path("Cyberpunk_2077").mkdir(parents=True, exist_ok=True)
+        Path(path).mkdir(parents=True, exist_ok=True)
+        files_in_directory = os.listdir(path)
         print('Start downloading Game files')
-        download_all_files(url=url, installer_base=installer_base, gog_al_cookie=gog_al_cookie, thread_amount=thread_amount)  # Download Game Files
+        download_all_files(url=url, installer_base=installer_base, gog_al_cookie=gog_al_cookie, thread_amount=thread_amount, files_in_directory=files_in_directory, path=path)  # Download Game Files
         print('All Game Installer Downloaded')
         print('Start downloading Patch files')
-        download_all_files(url=url, installer_base=patch_base, gog_al_cookie=gog_al_cookie, thread_amount=thread_amount)  # Download Patch Files
+        download_all_files(url=url, installer_base=patch_base, gog_al_cookie=gog_al_cookie, thread_amount=thread_amount, files_in_directory=files_in_directory, path=path)  # Download Patch Files
         print('All Patch Files')
         print('All Downloads Finished')
     else:
@@ -48,12 +50,30 @@ def get_download_file_url(url, gog_al_cookie):
     return r.url
 
 
-def download_one_installer(url: str, thread_id):
+def check_file_exists(path: str, file_name: str, files_in_directory):
+    to_search = []
+    if '.bin' in file_name:
+        if 'patch' in file_name:
+            to_search.append(file_name.split('Build')[0])
+        elif 'setup' in file_name:
+            to_search.append(file_name.split('build')[0])
+        to_search.append('-' + file_name.split('-')[1])
+    for file in files_in_directory:
+        counter = 0
+        for p in to_search:
+            if p in file:
+                counter += 1
+        if counter == len(to_search):
+            return True
+    return False
+
+
+def download_one_installer(url: str, thread_id, files_in_directory, path):
     file_name = get_file_name(url)
-    path = 'Cyberpunk_2077/' + str(file_name)
-    if os.path.exists(path):
+    if check_file_exists(path, file_name, files_in_directory):
         print(f'Thread {thread_id} File {file_name} already exists')
         return
+    path +='/' + str(file_name)
     r = requests.get(url=url, stream=True)
     print(f'Thread {thread_id} Downloading: ' + file_name)
     byte_counter = 0
@@ -74,7 +94,7 @@ def download_one_installer(url: str, thread_id):
         if os.path.exists(path):
             os.remove(path)
         time.sleep(60)
-        download_one_installer(url, thread_id)
+        download_one_installer(url, thread_id, files_in_directory, path)
     total_download_time = time.time() - download_start
     print(f'Thread {thread_id} Download completed. Total File Size: {convert_size(byte_counter)}. Total download time: {str(datetime.timedelta(seconds=total_download_time))}')
 
@@ -94,10 +114,10 @@ def convert_size(size_bytes):
     return "%s %s" % (s, size_name[i])
 
 
-def download_all_files(url, installer_base, gog_al_cookie, thread_amount):
+def download_all_files(url, installer_base, gog_al_cookie, thread_amount, files_in_directory, path):
     threads = []
     for id in range(0, thread_amount):
-        threads.append(DownloadThread(id, thread_amount, gog_al_cookie, installer_base, url))
+        threads.append(DownloadThread(id, thread_amount, gog_al_cookie, installer_base, url, files_in_directory, path))
     for thread in threads:
         thread.start()
     for thread in threads:
@@ -105,13 +125,15 @@ def download_all_files(url, installer_base, gog_al_cookie, thread_amount):
 
 
 class DownloadThread(threading.Thread):
-    def __init__(self, id, total_threads, gog_al_cookie, installer_base, url):
+    def __init__(self, id, total_threads, gog_al_cookie, installer_base, url, files_in_directory, path):
         threading.Thread.__init__(self)
         self.id = id
         self.total_threads = total_threads
         self.gog_al_cookie = gog_al_cookie
         self.installer_base = installer_base
         self.url = url
+        self.files_in_directory = files_in_directory
+        self.path = path
 
     def run(self):
         print(f'Thread {self.id} started')
@@ -126,7 +148,7 @@ class DownloadThread(threading.Thread):
             elif 'patch' in file_download_url and 'Build' not in file_download_url:
                 print(f'Thread {self.id} Found GOG Patch')
                 continue
-            download_one_installer(url=file_download_url, thread_id=self.id)
+            download_one_installer(url=file_download_url, thread_id=self.id, files_in_directory=self.files_in_directory, path=self.path)
         print(f'Thread {self.id} finished')
 
 
